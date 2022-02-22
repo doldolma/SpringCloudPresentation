0661eee5b4ca55aeb2dd73d35fede3af1af2d8fd
dolma <dolma@gridwiz.com>
2022-02-22 18:52:12 +0900

    docker 파일 작성

--- config-service/src/main/java/com/doldolma/configService/ConfigSererApplication.java
+++ config-service/src/main/java/com/doldolma/configService/ConfigSererApplication.java
@@ -1,4 +1,4 @@
-package com.doldolma.configSerer;
+package com.doldolma.configService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- discovery-service/src/main/java/com/doldolma/discoveryService/DiscoveryserviceApplication.java
+++ discovery-service/src/main/java/com/doldolma/discoveryService/DiscoveryserviceApplication.java
@@ -1,4 +1,4 @@
-package com.doldolma.discoveryservice;
+package com.doldolma.discoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
